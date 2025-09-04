04b7fdf9836b8115e98f1ef33ed20569602ed78a
atharva169 <atharvasinghh72@gmail.com>
2025-09-04 11:35:46 +0530

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -1,11 +1,11 @@
 class ATM:
     def __init__(self, starting_balance=100, pin=6969):
-        self.balance = starting_balance
-        self.pin = pin
+        self.balance= starting_balance
+        self.pin= pin
 
     def login(self):
-        entered_pin = int(input("Enter your ATM PIN: "))
-        if entered_pin == self.pin:
+        entered_pin= int(input("Enter your ATM PIN: "))
+        if entered_pin== self.pin:
             print("Login successful!\n")
             return True
         else:
@@ -20,16 +20,16 @@ class ATM:
             print("3. Withdraw Money")
             print("4. Change PIN")
             print("5. Exit")
-            option = input("Select an option (1-5): ")
-            if option == "1":
+            option= input("Select an option (1-5): ")
+            if option=="1":
                 self.check_balance()
-            elif option == "2":
+            elif option=="2":
                 self.deposit_money()
-            elif option == "3":
+            elif option=="3":
                 self.withdraw_money()
-            elif option == "4":
+            elif option=="4":
                 self.update_pin()
-            elif option == "5":
+            elif option=="5":
                 print("Thank you for using the ATM. ADIOS AMIGOS!!!")
                 break
             else:
@@ -39,29 +39,28 @@ class ATM:
         print(f"Your current balance is: Rs.{self.balance}")
 
     def deposit_money(self):
-        deposit = float(input("Enter amount to deposit: "))
-        if deposit > 0:
-            self.balance += deposit
+        deposit=float(input("Enter amount to deposit: "))
+        if deposit>0:
+            self.balance+= deposit
             print(f"Rs.{deposit} deposited successfully.")
         else:
             print("Deposit amount must be greater than 0.")
 
     def withdraw_money(self):
-        withdraw = float(input("Enter amount to withdraw: "))
-        if withdraw <= 0:
+        withdraw= float(input("Enter amount to withdraw: "))
+        if withdraw<= 0:
             print("Enter a valid withdrawal amount.")
-        elif withdraw <= self.balance:
-            self.balance -= withdraw
+        elif withdraw<=self.balance:
+            self.balance-= withdraw
             print(f"Rs.{withdraw} withdrawn successfully.")
         else:
             print("Insufficient balance.")
 
     def update_pin(self):
-        new_pin = int(input("Enter new 4-digit PIN: "))
-        self.pin = new_pin
+        new_pin=int(input("Enter new 4-digit PIN: "))
+        self.pin=new_pin
         print("PIN updated successfully.")
 
 atm = ATM() 
 if atm.login():
     atm.menu() 
-
